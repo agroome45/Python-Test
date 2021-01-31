a86c8571764a548f063d0c0485341f12a2fb4166
Alex Groome <agroome45@verizon.net>
2021-01-31 14:00:30 -0500

    Created functions and started DFS

--- test_Python/test/test.py
+++ test_Python/test/test.py
@@ -1,30 +1,48 @@
 import random
 from random import randint
 
+def create_maze(m,R):
+    matrix=[]
+    for i in range(m):
+        c=[]
+        for j in range(m):
+            rand = randint(0,100)
+            if(i == 0 and j == 0):
+                j = "S"
+            elif(i == (m-1) and j == (m-1)):
+                j = "E"
+            elif(rand > R):
+                j = "O"
+            else:
+                j = "X"
+            c.append(j)
+        matrix.append(c)
+    return matrix
+
+
+def print_maze(matrix,m):
+    for i in range(m):
+        for j in range(m):
+            print(matrix[i][j], end="    ")
+        print()
+    print()
+    
+def DFS(matrix, start_location, end_location):
+    fringe = []
+    fringe.append(start_location)
+    while(fringe.empty):
+        current = fringe.pop()
+        if(current == end_location):
+            return True
+        else:
+            i = 0
+            
+        
+    
+        
 m = int(input("Enter Size "))
 R = int(input("Enter Probability"))
 
-matrix=[]
-for i in range(m):
-    c=[]
-    for j in range(m):
-        rand = randint(0,100)
-        if(i == 0 and j == 0):
-            j = "S"
-        elif(i == (m-1) and j == (m-1)):
-            j = "E"
-        elif(rand > R):
-            j = "O"
-        else:
-            j = "X"
-        c.append(j)
-    matrix.append(c)
-
 print()
-
-for i in range(m):
-    for j in range(m):
-        print(matrix[i][j], end="    ")
-    print()
-        
-
+matrix = create_maze(m, R)
+print_maze(matrix, m)
