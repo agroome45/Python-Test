04137fa7d47faa9456dd06373310ae11f1d0ec74
Alex <agroome45@verizon.net>
2021-01-30 19:09:11 -0500

    New Matrix Code

--- test_Python/test/test.py
+++ test_Python/test/test.py
@@ -1,16 +1,5 @@
-m = int(input("Enter Size please test test"))
-Mat = []
-for i in range (0,m):
-    Mat.append([])
-for i in range (0,m):
-    for j in range(0,m):
-        Mat[i].append(j)
-        Mat[i][j]=0
-for i in range (0,m):
-    for j in range (0,m):
-        print ('entry in row: ', i+1, ' column: ', j+1)
-        Mat[i][j]= int(input())
-print(Mat)
+m = int(input("Enter Size please test te")
+
         
         
 
